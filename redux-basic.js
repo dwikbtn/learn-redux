558091ed39cab3304a7b7f18e3cd16a75ab064e1
Dwi Darma <dwikbtn15@gmail.com>
2021-02-23 15:34:22 +0800

    dispatching action and adding subscription

--- redux-basic.js
+++ redux-basic.js
@@ -24,7 +24,15 @@ const rootReducer = (state = initialState, action) => {
 //store
 const store = createStore(rootReducer);
 console.log(store.getState());
+//subscription
+store.subscribe(() => {
+  console.log("[subscription]", store.getState());
+});
 //dispatching action
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
+//subscription
+store.subscribe(() => {
+  console.log("[subscription]", store.getState());
+});
